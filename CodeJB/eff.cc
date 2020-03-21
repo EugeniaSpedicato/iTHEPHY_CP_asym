3a6a507e5d6332cc2240e67ce80d637d8261ce66
ghzut <jonah.blank@tu-dortmund.de>
2020-03-21 23:27:06 +0100

    test 6

--- CodeJB/eff.cc
+++ CodeJB/eff.cc
@@ -749,22 +749,46 @@ void eff(string dir, string sample, string polarisation)
     min_err = h_phi_reco_SPi->GetBinError(i);
     phi_plus = h_phi_reco_SPi->GetBinContent(size - i - 1);
     plus_err = h_phi_reco_SPi->GetBinError(size - i - 1);
-    h_phi_test_SPi->SetBinContent(size/2 - i - 1, abs(phi_min - phi_plus));
-    h_phi_test_SPi->SetBinError(size/2 - i - 1, sqrt(pow(min_err, 2.) + pow(plus_err, 2.)));
-
+    if(phi_min != 0 && phi_plus != 0)
+    {
+      h_phi_test_SPi->SetBinContent(size/2 - i - 1, abs(phi_min - phi_plus));
+      h_phi_test_SPi->SetBinError(size/2 - i - 1, sqrt(pow(min_err, 2.) + pow(plus_err, 2.)));
+    }
+    else
+    {
+      h_phi_test_SPi->SetBinContent(size/2 - i - 1, 0.);
+      h_phi_test_SPi->SetBinError(size/2 - i - 1, 0.);
+    }
+    
     phi_min = h_phi_reco_SPi_pos->GetBinContent(i);
     min_err = h_phi_reco_SPi_pos->GetBinError(i);
     phi_plus = h_phi_reco_SPi_pos->GetBinContent(size - i - 1);
     plus_err = h_phi_reco_SPi_pos->GetBinError(size - i - 1);
-    h_phi_test_SPi_pos->SetBinContent(size/2 - i - 1, abs(phi_min - phi_plus));
-    h_phi_test_SPi_pos->SetBinError(size/2 - i -1, sqrt(pow(min_err, 2.) + pow(plus_err, 2.)));
-
+    if(phi_min != 0 && phi_plus != 0)
+    {
+      h_phi_test_SPi_pos->SetBinContent(size/2 - i - 1, abs(phi_min - phi_plus));
+      h_phi_test_SPi_pos->SetBinError(size/2 - i - 1, sqrt(pow(min_err, 2.) + pow(plus_err, 2.)));
+    }
+    else
+    {
+      h_phi_test_SPi_pos->SetBinContent(size/2 - i - 1, 0.);
+      h_phi_test_SPi_pos->SetBinError(size/2 - i - 1, 0.);
+    }
+    
     phi_min = h_phi_reco_SPi_neg->GetBinContent(i);
     min_err = h_phi_reco_SPi_neg->GetBinError(i);
     phi_plus = h_phi_reco_SPi_neg->GetBinContent(size - i - 1);
     plus_err = h_phi_reco_SPi_neg->GetBinError(size - i - 1);
-    h_phi_test_SPi_neg->SetBinContent(size/2 - i - 1, abs(phi_min - phi_plus));
-    h_phi_test_SPi_neg->SetBinError(size/2 - i - 1, sqrt(pow(min_err, 2.) + pow(plus_err, 2.)));
+    if(phi_min != 0 && phi_plus != 0)
+    {
+      h_phi_test_SPi_neg->SetBinContent(size/2 - i - 1, abs(phi_min - phi_plus));
+      h_phi_test_SPi_neg->SetBinError(size/2 - i - 1, sqrt(pow(min_err, 2.) + pow(plus_err, 2.)));
+    }
+    else
+    {
+      h_phi_test_SPi_neg->SetBinContent(size/2 - i - 1, 0.);
+      h_phi_test_SPi_neg->SetBinError(size/2 - i - 1, 0.);
+    }
   }
   TCanvas *c_test = new TCanvas();
   h_phi_test_SPi->Draw();
