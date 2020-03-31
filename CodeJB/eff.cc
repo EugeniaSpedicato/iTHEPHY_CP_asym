30bdc1627b469759995b22031cfada7c30b74c83
ghzut <jonah.blank@tu-dortmund.de>
2020-03-31 14:59:38 +0200

    applying reasonable weights

--- CodeJB/eff.cc
+++ CodeJB/eff.cc
@@ -560,8 +560,8 @@ void eff(string dir, string sample, string polarisation)
   TF1 *y_func = new TF1("y_gaus","gaus(0)", -0.3, 0.);
   TF1 *z_func = new TF1("z_gaus","gaus(0)", -130., 130.);
   
-  x_func->SetParameters(x_N, x_mean - 1.5*x_sig, x_sig);
-  y_func->SetParameters(y_N, y_mean + 1.5*y_sig, y_sig);
+  x_func->SetParameters(x_N, x_mean - 1.25*x_sig, x_sig*0.8);
+  y_func->SetParameters(y_N, y_mean + 1.25*y_sig, y_sig*0.8);
   z_func->SetParameters(z_N, 0., z_sig);
 
   
