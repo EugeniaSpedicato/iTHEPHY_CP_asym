8188ece9128088f18df0aa33ae84f2c9f8f52b10
ghzut <33569260+ghzut@users.noreply.github.com>
2020-04-26 23:07:50 +0200

    m->n

--- CodeJB/data.cc
+++ CodeJB/data.cc
@@ -7,8 +7,8 @@ void data(string dir, string sample)
   uint64_t start_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
   string input_name = dir+"/"+sample+".root";
   TChain *ntp = new TChain();
-  ntp->AddFile(input_name.c_str(),"mtp;25");
-  ntp->AddFile(input_name.c_str(),"mtp;26");
+  ntp->AddFile(input_name.c_str(),"ntp;25");
+  ntp->AddFile(input_name.c_str(),"ntp;26");
 
   int nEvents = ntp->GetEntries();
 
