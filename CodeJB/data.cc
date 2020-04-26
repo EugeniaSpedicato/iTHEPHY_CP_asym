68fd25f5e92c5cde74072554d2e496e5c8dc5f49
ghzut <33569260+ghzut@users.noreply.github.com>
2020-04-27 00:43:55 +0200

    Mass dependency

--- CodeJB/data.cc
+++ CodeJB/data.cc
@@ -1,6 +1,15 @@
 #include "eff.h"
 #include <chrono>
 
+void printhists(TH1F *h)
+{
+  TCanvas *c = new TCanvas();
+  h->Draw();
+  h->Draw("hist same");
+  string title_name = h->GetName();
+  string save_name = "output/data/plots"+title_name+".pdf";
+  c->SaveAs(save_name.c_str());
+}
 
 void data(string dir, string sample)
 {
@@ -11,13 +20,87 @@ void data(string dir, string sample)
   ntp->AddFile(input_name.c_str(),-1,"ntp;26");
 
   int nEvents = ntp->GetEntries();
+  double nDst_pos = 0.; double nDst_neg = 0.;
+  int Dst_ID, D0_ID, Pi_ID, K_ID, SPi_ID;
+  double D0_mass;
+  double DTF_mass;
 
-  cout << "Number of events: " << nEvents << endl;
+
+  ntp->SetBranchStatus("D0_M",1); ntp->SetBranchAddress("D0_M", &(D0_mass));
+  ntp->SetBranchStatus("DTF_Mass",1); ntp->SetBranchAddress("DTF_Mass", &(DTF_mass));
+
+  ntp->SetBranchStatus("Dst_ID",1); ntp->SetBranchAddress("Dst_ID", &(Dst_ID));
+  ntp->SetBranchStatus("D0_ID",1); ntp->SetBranchAddress("D0_ID", &(D0_ID));
+  ntp->SetBranchStatus("P1_ID",1); ntp->SetBranchAddress("P1_ID", &(Pi_ID));
+  ntp->SetBranchStatus("P2_ID",1); ntp->SetBranchAddress("P2_ID", &(K_ID));
+  ntp->SetBranchStatus("Dst_ID",1); ntp->SetBranchAddress("Dst_ID", &(SPi_ID));
 
 
+  TH1F *h_Dst_pos_D0m = new TH1F("h_Dst_pos_D0m", ";invariant D0 mass/MeV; Events", 92, 1842., 1888.);
+  TH1F *h_Dst_neg_D0m = new TH1F("h_Dst_neg_D0m", ";invariant D0 mass/MeV; Events", 92, 1842., 1888.);
+  TH1F *h_Dst_asym_D0m = new TH1F("h_Dst_asym_D0m", ";invariant D0 mass/MeV; assymmetry", 92, 1842., 1888.);
 
+  h_Dst_pos_D0m->Sumw2();
+  h_Dst_neg_D0m->Sumw2();
+  h_Dst_asym_D0m->Sumw2();
 
+  TH1F *h_Dst_pos_DTFm = new TH1F("h_Dst_pos_DTFm", ";invariant DTF mass/MeV; Events", 85, 2004., 2021.);
+  TH1F *h_Dst_neg_DTFm = new TH1F("h_Dst_neg_DTFm", ";invariant DTF mass/MeV; Events", 85, 2004., 2021.);
+  TH1F *h_Dst_asym_DTFm = new TH1F("h_Dst_asym_DTFm", ";invariant DTF mass/MeV; assymmetry", 85, 2004., 2021.);
 
+  h_Dst_pos_DTFm->Sumw2();
+  h_Dst_neg_DTFm->Sumw2();
+  h_Dst_asym_DTFm->Sumw2();
+
+  for (int i = 0; i < nEvents; ++i)
+  {
+    ntp->GetEvent(i);
+    if(Dst_ID < 0)
+    {
+      h_Dst_neg_D0m->Fill(D0_mass);
+      h_Dst_neg_DTFm->Fill(DTF_mass);
+      ++nDst_neg;
+    }
+    else
+    {
+      h_Dst_pos_D0m->Fill(D0_mass);
+      h_Dst_pos_DTFm->Fill(DTF_mass);
+      ++nDst_pos;
+    }
+  }
+  double Dst_asym = (nDst_pos-nDst_neg)/(nDst_pos+nDst_neg);
+  cout << "The total Dst assymmetry is: " << Dst_asym << endl;
+
+  printhists(h_Dst_pos_D0m);
+  printhists(h_Dst_neg_D0m);
+  printhists(h_Dst_pos_DTFm);
+  printhists(h_Dst_neg_DTFm);
+
+  string output_hist_name;
+  output_hist_name = "output/data/histOut_"+sample+".root";
+  TFile *out_hist_fi = new TFile(output_hist_name.c_str(),"RECREATE");
+
+  h_Dst_pos_D0m->Write();
+  h_Dst_neg_D0m->Write();
+  h_Dst_pos_DTFm->Write();
+  h_Dst_neg_DTFm->Write();
+
+  h_Dst_pos_D0m->Add(h_Dst_neg_D0m,-1);
+  h_Dst_neg_D0m->Scale(2.);
+  h_Dst_neg_D0m->Add(h_Dst_pos_D0m);
+  h_Dst_asym_D0m = h_Dst_pos_D0m->Divide(h_Dst_neg_D0m);
+  printhists(h_Dst_asym_D0m);
+
+  h_Dst_pos_DTFm->Add(h_Dst_neg_DTFm,-1);
+  h_Dst_neg_DTFm->Scale(2.);
+  h_Dst_neg_DTFm->Add(h_Dst_pos_DTFm);
+  h_Dst_asym_DTFm = h_Dst_pos_DTFm->Divide(h_Dst_neg_DTFm);
+  printhists(h_Dst_asym_DTFm);
+
+  h_Dst_asym_D0m->Write();
+  h_Dst_asym_DTFm->Write();
+  out_hist_fi->Write();
+  out_hist_fi->Close();
 
   uint64_t end_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
   float elapsed = (end_time - start_time)*0.000001;
