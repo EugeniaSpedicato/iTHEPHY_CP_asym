21bded9fe5e1b53a1abfac720b3fd3d6bac854de
ghzut <33569260+ghzut@users.noreply.github.com>
2020-05-08 18:42:47 +0200

    low

--- CodeJB/data.cc
+++ CodeJB/data.cc
@@ -272,7 +272,7 @@ data6->plotOn(neg_sides_frame);
   arg_neg->paramOn(neg_low_frame, RooFit::Label("Fit Results"), RooFit::Format("NEU", RooFit::AutoPrecision(1)), RooFit::Layout(0.5,0.9,0.8));
 */
   neg_gr_frame->Draw();
-  canvas2->SaveAs("output/data/plots/neg_gr_fit.pdf");
+  canvas2->SaveAs("output/data/plots/neg_low_fit.pdf");
 
 
 
