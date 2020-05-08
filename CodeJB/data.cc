f66989ce7cd9626984a2a40c72befa4fc0f17d90
ghzut <33569260+ghzut@users.noreply.github.com>
2020-05-08 18:49:23 +0200

    more range

--- CodeJB/data.cc
+++ CodeJB/data.cc
@@ -253,7 +253,7 @@ data6->plotOn(neg_sides_frame);
   canvas2->SaveAs("output/data/plots/neg_sides.pdf");*/
 
   RooRealVar *m0_neg = new RooRealVar("m0_neg", "m0_neg", 2010, 2004., 2021.);
-  RooRealVar *c0_neg = new RooRealVar("c0_neg", "c0_neg", -3., -20., 0.);
+  RooRealVar *c0_neg = new RooRealVar("c0_neg", "c0_neg", -10., -30., 0.);
   RooRealVar *p_neg = new RooRealVar("p_neg", "p_neg", 2., -10., 10.);
   /*RooRealVar *m0_pos = new RooRealVar("m0_pos", "m0_pos", 2020, 2005., 2021.);
   RooRealVar *c0_pos = new RooRealVar("c0_pos", "c0_pos", -3., -10., 10.);
