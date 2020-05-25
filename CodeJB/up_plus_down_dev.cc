6c3df4368dc6af38f99e8386a97db32838685a12
ghzut <jonah.blank@tu-dortmund.de>
2020-05-25 17:12:15 +0200

    old plots again

--- CodeJB/up_plus_down_dev.cc
+++ CodeJB/up_plus_down_dev.cc
@@ -1,4 +1,59 @@
 #include "eff.h"
+
+void up_plus_down_dev()
+{
+  TFile f_up("output/histOut_minisample_Dst2D0pi_D02Kpi_2016_Up_GEN.root");
+  TFile f_dw("output/histOut_minisample_Dst2D0pi_D02Kpi_2016_Dw_GEN.root");
+
+  vector <TH1F*> v_h_up_eta_pos = {(TH1F*)f_up.Get("h_eta_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_K_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_Dst_pos_dev")};
+
+  vector <TH1F*> v_h_up_pT_pos = {(TH1F*)f_up.Get("h_pT_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_K_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_Dst_pos_dev")};
+
+  vector <TH1F*> v_h_up_phi_pos = {(TH1F*)f_up.Get("h_phi_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_K_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_Dst_pos_dev")};
+
+  vector <TH1F*> v_h_up_theta_pos = {(TH1F*)f_up.Get("h_theta_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_K_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_Dst_pos_dev")};
+
+
+  vector <TH1F*> v_h_dw_eta_pos = {(TH1F*)f_up.Get("h_eta_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_K_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_eta_reco_Dst_pos_dev")};
+
+  vector <TH1F*> v_h_dw_pT_pos = {(TH1F*)f_up.Get("h_pT_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_K_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_pT_reco_Dst_pos_dev")};
+
+  vector <TH1F*> v_h_dw_phi_pos = {(TH1F*)f_up.Get("h_phi_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_K_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_phi_reco_Dst_pos_dev")};
+
+  vector <TH1F*> v_h_dw_theta_pos = {(TH1F*)f_up.Get("h_theta_reco_Pi_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_K_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_SPi_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_D0_pos_dev"), (TH1F*)f_up.Get("h_theta_reco_Dst_pos_dev")};
+ 
+  int size = v_h_dw_eta_pos.size();
+  string save_name;
+  TCanvas *c = new TCanvas();
+  gStyle->SetOptStat(0);
+  TF1 func("0*x", "0*x", -5., 9600.);
+  for (int i =0; i < size;++i)
+  {
+    v_h_up_eta_pos.at(i)->Add(v_h_dw_eta_pos.at(i));
+    save_name = "output/up_plus_down_pdf/eta_"+to_string(i)+".pdf";
+    v_h_up_eta_pos.at(i)->Draw();
+    func.Draw("same");
+    c->SaveAs(save_name.c_str());
+    v_h_up_pT_pos.at(i)->Add(v_h_dw_pT_pos.at(i));
+    save_name = "output/up_plus_down_pdf/pT_"+to_string(i)+".pdf";
+    v_h_up_pT_pos.at(i)->Draw();
+    func.Draw("same");
+    c->SaveAs(save_name.c_str());
+    v_h_up_phi_pos.at(i)->Add(v_h_dw_phi_pos.at(i));
+    save_name = "output/up_plus_down_pdf/phi_"+to_string(i)+".pdf";
+    v_h_up_phi_pos.at(i)->Draw();
+    func.Draw("same");
+    c->SaveAs(save_name.c_str());
+    v_h_up_theta_pos.at(i)->Add(v_h_dw_theta_pos.at(i));
+    save_name = "output/up_plus_down_pdf/theta_"+to_string(i)+".pdf";
+    v_h_up_theta_pos.at(i)->Draw();
+    func.Draw("same");
+    c->SaveAs(save_name.c_str());
+  }
+}
+
+/*
+ *#include "eff.h"
 void printdevhists(vector<TH1F*> v_get_hist_pos, vector<TH1F*> v_get_hist_neg, string var)
 {
   TF1 func("0*x", "0*x", -5., 11000.);
@@ -30,11 +85,11 @@ void printdevhists(vector<TH1F*> v_get_hist_pos, vector<TH1F*> v_get_hist_neg, s
     v_hist_neg.at(i)->Scale(2.);
     v_hist_neg.at(i)->Add(v_hist_pos.at(i));
     v_hist_pos.at(i)->Divide(v_hist_neg.at(i));
-    /*if ( size > 4 && i >= 4 )
+    if ( size > 4 && i >= 4 )
     {
       v_hist_pos.at(i)->SetAxisRange(-0.02, 0.02, "Y");
     }
-    else v_hist_pos.at(i)->SetAxisRange(-0.15, 0.15, "Y");*/
+    else v_hist_pos.at(i)->SetAxisRange(-0.15, 0.15, "Y");
     v_hist_pos.at(i)->Draw();
     func.Draw("same");
     save_name = "output/up_plus_down_pdf/"+var+"_"+to_string(i)+".pdf";
@@ -102,3 +157,4 @@ void up_plus_down_dev()
   printdevhists(v_h_up_theta_pos,v_h_up_theta_neg, "theta");
 }
 
+ */
